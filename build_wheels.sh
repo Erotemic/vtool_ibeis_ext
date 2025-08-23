18187dcd7cb8aaa3883cd9dc09f1f72a55a50535
joncrall <erotemic@gmail.com>
2025-08-23 19:49:18 -0400

    update build backend

--- build_wheels.sh
+++ build_wheels.sh
@@ -19,6 +19,9 @@ if ! which cibuildwheel ; then
 fi
 
 
-#pip wheel -w wheelhouse .
-# python -m build --wheel -o wheelhouse  #  vtool_ibeis_ext: +COMMENT_IF(binpy)
-cibuildwheel --config-file pyproject.toml --platform linux --arch x86_64  #  vtool_ibeis_ext: +UNCOMMENT_IF(binpy)
+LOCAL_CP_VERSION=$(python3 -c "import sys; print('cp' + ''.join(list(map(str, sys.version_info[0:2]))))")
+echo "LOCAL_CP_VERSION = $LOCAL_CP_VERSION"
+
+# Build for only the current version of Python
+export CIBW_BUILD="${LOCAL_CP_VERSION}-*"
+cibuildwheel --config-file pyproject.toml --platform linux --archs x86_64
